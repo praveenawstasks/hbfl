559f52f73ea6e4e99e2dbc2b0985d0c9a596e694
PraveenLokam <praveen1.lokam@gmail.com>
2022-06-29 15:45:44 -0600

    market ami changes

--- scripts/03/marketplace-ec2-startup.sh
+++ scripts/03/marketplace-ec2-startup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install git
-rm -rf /home/bitnami/hbfl
+sudo rm -rf /home/bitnami/hbfl
 git clone https://github.com/praveenawstasks/hbfl.git /home/bitnami/hbfl
 chown -R bitnami: /home/bitnami/hbfl
 cd /home/bitnami/hbfl
+sudo rm -rf node_modules
 sudo npm ci
 sudo npm run start
 
 # The above commands base64 encoded for entering into UserData
-# IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0Kcm0gLXJmIC9ob21lL2JpdG5hbWkvaGJmbA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9wcmF2ZWVuYXdzdGFza3MvaGJmbC5naXQgL2hvbWUvYml0bmFtaS9oYmZsDQpjaG93biAtUiBiaXRuYW1pOiAvaG9tZS9iaXRuYW1pL2hiZmwNCmNkIC9ob21lL2JpdG5hbWkvaGJmbA0Kc3VkbyBucG0gY2kNCnN1ZG8gbnBtIHJ1biBzdGFydA0K
+# IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0Kc3VkbyBybSAtcmYgL2hvbWUvYml0bmFtaS9oYmZsDQpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL3ByYXZlZW5hd3N0YXNrcy9oYmZsLmdpdCAvaG9tZS9iaXRuYW1pL2hiZmwNCmNob3duIC1SIGJpdG5hbWk6IC9ob21lL2JpdG5hbWkvaGJmbA0KY2QgL2hvbWUvYml0bmFtaS9oYmZsDQpzdWRvIHJtIC1yZiBub2RlX21vZHVsZXMNCnN1ZG8gbnBtIGNpDQpzdWRvIG5wbSBydW4gc3RhcnQ=
